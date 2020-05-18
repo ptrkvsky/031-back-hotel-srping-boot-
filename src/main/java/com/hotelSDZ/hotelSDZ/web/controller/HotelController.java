02a8bcd7b915c159e57c03e2693b4246e7288ea8
Johan Gitlab <johan.petrikovsky@gmail.com>
2020-05-18 18:19:35 +0200

    update adress controller

--- src/main/java/com/hotelSDZ/hotelSDZ/web/controller/HotelController.java
+++ src/main/java/com/hotelSDZ/hotelSDZ/web/controller/HotelController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://178.62.113.122")
 @RestController
 public class HotelController {
     @Autowired
